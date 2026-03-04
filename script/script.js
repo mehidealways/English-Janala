de8af3b36bb4ea99405317191b7d9a9b4cbfee78
mehidealways <mehidehasanmilon444@.com>
2026-03-05 01:24:03 +0600

    I did desing and add sum funtionality

--- script/script.js
+++ script/script.js
@@ -4,17 +4,54 @@ const loadLessons = () => {
     .then(json => displayLessons(json.data));
 };
 
+const loadLevelWord = (id) => {
+  // console.log(id);
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data));
+};
+const displayLevelWord = words => {
+  console.log(words);
+  const wordContainer = document.getElementById('wordContainer');
+  wordContainer.innerHTML = '';
+
+  words.forEach(word => {
+    // console.log(word);
+    const card = document.createElement('div');
+    card.innerHTML = `
+   <div class="bg-white rounded-md shadow-sm text-center p-10 space-y-4">
+  <h2 class="font-bold text-2xl">${word.word}</h2>
+  <p class="font-semibold">meaning / pronunciation</p>
+  
+  <div class="text-2xl font-medium font-bangla">
+  "${word.meaning} / ${word.pronunciation}"
+  </div>
+  
+  <div class="flex justify-between items-center ">
+  <button class="btn bg-[#1a91ff1a]"><i class="fa-solid fa-circle-info"></i><button>
+    <button class="btn bg-[#1a91ff1a]"><i class="fa-solid fa-volume-high"></i></button>
+  </div>
+
+  </div>
+  
+  `;
+    wordContainer.append(card);
+  });
+};
+
+// Vocabularies lessons
 const displayLessons = lessons => {
   // 1. get the container
   const levelContainer = document.getElementById('level-container');
   levelContainer.innerHTML = '';
   //2.ge into every lessons
   lessons.forEach(lesson => {
-    console.log(lesson);
+    // console.log(lesson);
     // 3.createElement
     const btnDive = document.createElement('div');
     btnDive.innerHTML = `
-   <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+   <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
     `;
     //  4. append
     levelContainer.append(btnDive);
@@ -24,3 +61,4 @@ const displayLessons = lessons => {
 
 // Calling Function
 loadLessons();
+
