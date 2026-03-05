94b3eea3a61ca5f751dcdadaaee34da51496e6df
mehidealways <mehidehasanmilon444@.com>
2026-03-06 01:46:31 +0600

    Add modal Functionality then add Spinner and Fixed Some Bugs

--- script/script.js
+++ script/script.js
@@ -1,3 +1,21 @@
+const createElements = arr => {
+  const htmlElements = arr.map(el => `<span class="btn">${el}</span>`);
+  return htmlElements.join(' ');
+};
+
+// Loader
+const manageLoader = (status) => {
+  // console.log(status);
+  if (status == true) {
+    document.getElementById('loader').classList.remove('hidden')
+    document.getElementById('wordContainer').classList.add('hidden')
+  }
+  else {
+     document.getElementById('wordContainer').classList.remove('hidden');
+     document.getElementById('loader').classList.add('hidden');
+  }
+}
+
 const loadLessons = () => {
   fetch('https://openapi.programming-hero.com/api/levels/all')
     .then(res => res.json())
@@ -6,25 +24,60 @@ const loadLessons = () => {
 
 // lession btn color add and remove
 const removeActive = () => {
-  const lessonButton = document.querySelectorAll('.lession-btn')
+  const lessonButton = document.querySelectorAll('.lession-btn');
   // console.log(lessonButton);
   lessonButton.forEach(btn => {
-    btn.classList.remove('active')
-  })
-}
+    btn.classList.remove('active');
+  });
+};
 
-const loadLevelWord = (id) => {
+const loadLevelWord = id => {
   // console.log(id);
+  manageLoader(true)
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      removeActive()
+      removeActive();
       const clickBtn = document.getElementById(`lessonBtn-${id}`);
-      clickBtn.classList.add('active')
+      clickBtn.classList.add('active');
       // clickBtn.className.remove()
       displayLevelWord(data.data);
     });
+};
+
+// Modal Details
+const loadWordDetail = async id => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  // console.log(url);
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+const displayWordDetails = word => {
+  console.log(word);
+  const detailsBox = document.getElementById('detailsContainer');
+  detailsBox.innerHTML = `
+       <div class="">
+        <h2 class="text-2xl font-bold">${word.word}(<i class="fa-solid fa-microphone"></i>:${word.pronunciation})</h2>
+      </div>
+
+      <div class="">
+        <h2 class=" font-bold">Meaning</h2>
+        <p>${word.meaning}</p>
+      </div>
+
+      <div class="">
+        <h2 class=" font-bold">Example</h2>
+        <p>${word.sentence}</p>
+      </div>
+
+      <div class="">
+        <h2 class="mb-4 font-bold">Synonym</h2>
+        <div class="">${createElements(word.synonyms)}</div>
+      </div>
+  `;
+  document.getElementById('word_modal').showModal();
 };
 const displayLevelWord = words => {
   console.log(words);
@@ -32,14 +85,13 @@ const displayLevelWord = words => {
   wordContainer.innerHTML = '';
   // Condition
   if (words.length == 0) {
-      wordContainer.innerHTML = `<div class="text-center col-span-full space-y-5 font-bangla ">
+    wordContainer.innerHTML = `<div class="text-center col-span-full space-y-5 font-bangla ">
   <img class = " mx-auto" src="./assets/alert-error.png" alt="">
   <p class="text-xl text-gray-500 font-semibold">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
   <h2 class="text-2xl font-semibold text-gray-500">নেক্সট Lesson এ যান।</h2>
 </div>`;
-    return
+    return;
   }
-
 
   words.forEach(word => {
     // console.log(word);
@@ -54,7 +106,7 @@ const displayLevelWord = words => {
   </div>
   
   <div class="flex justify-between items-center ">
-  <button class="btn bg-[#1a91ff1a]"><i class="fa-solid fa-circle-info"></i><button>
+  <button onclick ="loadWordDetail(${word.id})" class="btn bg-[#1a91ff1a]"><i class="fa-solid fa-circle-info"></i><button>
     <button class="btn bg-[#1a91ff1a]"><i class="fa-solid fa-volume-high"></i></button>
   </div>
 
@@ -63,6 +115,7 @@ const displayLevelWord = words => {
   `;
     wordContainer.append(card);
   });
+  manageLoader(false)
 };
 
 // Vocabularies lessons
@@ -83,7 +136,5 @@ const displayLessons = lessons => {
   });
 };
 
-
 // Calling Function
 loadLessons();
-
